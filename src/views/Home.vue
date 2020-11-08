8e0a14af8d1d35856563cab4c365c0114f174772
Kristijan Cetina <kristijan.cetina@gmail.com>
2020-11-08 10:31:05 +0100

    added login & signup pages

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to fipugram by Kristijan Cetina"/>
   </div>
 </template>
 
---
+++ src/views/Signup.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="login">
+        <form>
+            <h1>yo Slim Shady, please sign up</h1>
+
+            <div class="form-group">
+                <label>Full Name</label>
+                <input type="text" class="form-control"/>
+            </div>
+
+            <div class="form-group">
+                <label>Email address</label>
+                <input type="email" class="form-control" />
+            </div>
+
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" class="form-control" />
+            </div>
+
+            <button type="submit" class="btn btn-primary">Sign Up</button>
+
+            <p class="forgot-password text-right">
+                Already registered? 
+                <router-link :to="{name: 'Login'}">sign in</router-link>
+            </p>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        }
+    }
+</script>
