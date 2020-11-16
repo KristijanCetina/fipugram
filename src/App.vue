e372d35a7d38a1bcaf2f36cadec1136c914916e9
Kristijan Cetina <kristijan.cetina@gmail.com>
2020-11-16 21:14:14 +0100

    Komponente, Navbar i Home screen

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,51 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/login">Prijava</router-link> |
-      <router-link to="/signup">Registracija</router-link>
-    </div>
-    <router-view/>
+    <nav id="nav" class="navbar navbar-expand-lg navbar-light">
+      <a class="navbar-brand" href="/">
+        <img
+          src="@/assets/kclogo.png"
+          height="40"
+          class="d-inline-block align-top"
+          alt="Nas logotip kompanije koji prikazuje ronioca na dah kako zaranja"
+          loading="lazy"
+        />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarToggler"
+        aria-controls="navbarToggler"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarToggler">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item"><router-link to="/">Home</router-link></li>
+          <li class="nav-item">
+            <router-link to="/login">Prijava</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/signup">Registracija</router-link>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input
+            class="form-control mr-sm-2"
+            type="search"
+            placeholder="Pretraga"
+            aria-label="Search"
+          />
+        </form>
+      </div>
+    </nav>
+    <router-view />
   </div>
 </template>
+
+// <li class="nav-item"><router-link to="/">Home</router-link></li>
 
 <style lang="scss">
 #app {
@@ -24,10 +62,14 @@
   a {
     font-weight: bold;
     color: #2c3e50;
+    padding-right: 10px;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: #0086cc;
     }
   }
+}
+a {
+  color: #0086cc;
 }
 </style>
