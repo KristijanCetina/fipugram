0c6dd78ee97d80bb082184b12a87684e664c8621
Kristijan Cetina <kristijan.cetina@gmail.com>
2020-11-19 17:54:07 +0100

    first step with stripe payment gateway

---
+++ src/views/Payment.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="row">
+    <div class="col-2"></div>
+    <div class="col-7">
+      <div class="container">
+        <button class="btn btn-primary" id="checkout-button">Checkout</button>
+      </div>
+    </div>
+    <div class="col-3"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
