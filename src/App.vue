dbf544629bcc57bcd5a5bfee963102e52199d539
Kristijan Cetina <KristijanCetina@users.noreply.github.com>
2020-12-23 22:11:31 +0100

    dodan (kvazi-obavezan) login

--- src/App.vue
+++ src/App.vue
@@ -1,89 +1,121 @@
 <template>
-  <div id="app">
-    <nav id="nav" class="navbar navbar-expand-lg navbar-light">
-      <a class="navbar-brand" href="/">
-        <img
-          src="@/assets/kclogo.png"
-          height="40"
-          class="d-inline-block align-top"
-          alt="Nas logotip kompanije koji prikazuje nešto cool"
-          loading="lazy"
-        />
-      </a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarToggler"
-        aria-controls="navbarToggler"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarToggler">
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-          <li class="nav-item"><router-link to="/">Home</router-link></li>
-          <li class="nav-item">
-            <router-link to="/login">Prijava</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/signup">Registracija</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/payment">Payment</router-link>
-          </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input
-            v-model="store.searchTerm"
-            class="form-control mr-sm-2"
-            type="search"
-            placeholder="Pretraga"
-            aria-label="Search"
-          />
-        </form>
-      </div>
-    </nav>
-    <router-view />
-  </div>
+	<div id="app">
+		<nav id="nav" class="navbar navbar-expand-lg navbar-light">
+			<a class="navbar-brand" href="/">
+				<img
+					src="@/assets/kclogo.png"
+					height="40"
+					class="d-inline-block align-top"
+					alt="Nas logotip kompanije koji prikazuje nešto cool"
+					loading="lazy"
+				/>
+			</a>
+			<button
+				class="navbar-toggler"
+				type="button"
+				data-toggle="collapse"
+				data-target="#navbarToggler"
+				aria-controls="navbarToggler"
+				aria-expanded="false"
+				aria-label="Toggle navigation"
+			>
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarToggler">
+				<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+					<li class="nav-item">
+						<router-link to="/">Home</router-link>
+					</li>
+					<li v-if="!store.currentUser" class="nav-item">
+						<router-link to="/login">Prijava</router-link>
+					</li>
+					<li v-if="!store.currentUser" class="nav-item">
+						<router-link to="/signup">Registracija</router-link>
+					</li>
+					<li v-if="store.currentUser" class="nav-item">
+						<router-link to="/payment">Payment</router-link>
+					</li>
+					<li v-if="store.currentUser" class="nav-item">
+						<a href="#" @click.prevent="logout()">Logout</a>
+					</li>
+				</ul>
+				<form class="form-inline my-2 my-lg-0">
+					<input
+						v-model="store.searchTerm"
+						class="form-control mr-sm-2"
+						type="search"
+						placeholder="Pretraga"
+						aria-label="Search"
+					/>
+				</form>
+			</div>
+		</nav>
+		<router-view />
+	</div>
 </template>
 
 <script>
-import store from "@/store"
+import store from "@/store";
+import { firebase } from "@/firebase";
+import router from "@/router";
+
+firebase.auth().onAuthStateChanged((user) => {
+	if (user) {
+		// User is signed in.
+		store.currentUser = user.email;
+	} else {
+		// No user is signed in.
+		store.currentUser = null;
+
+		if (router.name != "Login") {
+			router.push({ name: "Login" });
+		}
+	}
+});
+
 export default {
-  name: "app",
-  data() {
-    return {
-      store,
-    };
-  },
+	name: "app",
+	data() {
+		return {
+			store,
+		};
+	},
+	methods: {
+		logout() {
+			firebase
+				.auth()
+				.signOut()
+				.then(() => {
+					this.$router.push({ name: "Login" });
+				});
+		},
+	},
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+	padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    padding-right: 10px;
+	a {
+		font-weight: bold;
+		color: #2c3e50;
+		padding-right: 10px;
 
-    &.router-link-exact-active {
-      color: #0086cc;
-    }
-  }
+		&.router-link-exact-active {
+			color: #0086cc;
+		}
+	}
 }
 a {
-  color: #0086cc;
+	color: #0086cc;
 }
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,42 +1,75 @@
 <template>
-  <div class="about">
-    <h1>This is the login page</h1>
-    <div class="container">
-      <div class="row">
-        <div class="col-sm"></div>
-        <div class="col-sm">
-          <form>
-            <div class="form-group">
-              <label for="exampleInputEmail1">Email address</label>
-              <input
-                type="email"
-                class="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                placeholder="Enter email"
-              />
-              <small id="emailHelp" class="form-text text-muted"
-                >We'll never share your email with anyone else.</small
-              >
-            </div>
-            <div class="form-group">
-              <label for="exampleInputPassword1">Password</label>
-              <input
-                type="password"
-                class="form-control"
-                id="exampleInputPassword1"
-                placeholder="Password"
-              />
-            </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
-            <p class="forgot-password">
-              You don't have an account?
-              <router-link :to="{ name: 'Signup' }">sign up</router-link>
-            </p>
-          </form>
-        </div>
-        <div class="col-sm"></div>
-      </div>
-    </div>
-  </div>
+	<div class="about">
+		<h1>This is the login page</h1>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm"></div>
+				<div class="col-sm">
+					<form>
+						<div class="form-group">
+							<label for="exampleInputEmail1">Email address</label>
+							<input
+								v-model="email"
+								type="email"
+								class="form-control"
+								id="exampleInputEmail1"
+								aria-describedby="emailHelp"
+								placeholder="Enter email"
+							/>
+							<small id="emailHelp" class="form-text text-muted"
+								>We'll never share your email with anyone else.</small
+							>
+						</div>
+						<div class="form-group">
+							<label for="exampleInputPassword1">Password</label>
+							<input
+								v-model="password"
+								type="password"
+								class="form-control"
+								id="exampleInputPassword1"
+								placeholder="Password"
+							/>
+						</div>
+						<button type="button" @click="login()" class="btn btn-primary">
+							Submit
+						</button>
+						<p class="forgot-password">
+							You don't have an account?
+							<router-link :to="{ name: 'Signup' }">sign up</router-link>
+						</p>
+					</form>
+				</div>
+				<div class="col-sm"></div>
+			</div>
+		</div>
+	</div>
 </template>
+
+<script>
+import { firebase } from "@/firebase";
+export default {
+	name: "login",
+	data() {
+		return {
+			email: "",
+			password: "",
+		};
+	},
+	methods: {
+		login() {
+			console.log("login..." + this.email);
+			firebase
+				.auth()
+				.signInWithEmailAndPassword(this.email, this.password)
+				.then((result) => {
+					this.$router.replace({ name: "Home" });
+				})
+				.catch(function (e) {
+					console.error(e);
+				});
+		},
+	},
+};
+</script>
+
+
