70aee1e7843c5c348703f4f9b12a443c9b395d7c
Kristijan Cetina <kristijan.cetina@gmail.com>
2021-01-23 16:14:49 +0100

    dodan upload slike

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import Croppa from "vue-croppa";
+import "vue-croppa/dist/vue-croppa.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(Croppa);
 
 new Vue({
-    router,
-    store,
-    render: (h) => h(App)
-}).$mount('#app')
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
